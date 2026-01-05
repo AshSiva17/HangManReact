ec19c120191cedba4e9cb7d463f6d8edd96ac77a
Ashwin Sivabalan <sivabalanashwin@gmail.com>
2026-01-04 19:53:07 -0500

    Add Status section

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ export default function Hangman() {
                 <h1>Assembley:Endgame</h1>
                 <h3>Guess the word in under 8 attempts to keep the programming world safe from Assembly</h3>
             </header>
+
+            <section className="gameStatus">
+                <h3>You Win!</h3>
+                <p>Well Done 🎉</p>
+            </section>
         </main>
     )
 }
